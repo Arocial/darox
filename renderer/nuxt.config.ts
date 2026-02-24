058202e15cb2982890b236f168b613bf4777dd1b
Solaria6 <solariar@outlook.com>
2026-02-24 17:26:51 +0800

    Remove renderer by ai-sdk/vue

--- renderer/nuxt.config.ts
+++
@@ -1,8 +0,0 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
-
-  compatibilityDate: '2024-07-05',
-  telemetry: false,
-});
